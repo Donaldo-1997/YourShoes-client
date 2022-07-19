c9aeebc893e1d9c5ec42267f76ef21a588fb9ec0
Juan Cruz Carracedo <juann.carracedo7@gmail.com>
2022-07-19 17:51:02 -0300

    Navbar, Banner, Searchbar, LogIn, esqueleto

---
+++ src/components/Banner/Banner.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+export default function Banner(){
+
+    return(
+        
+            <div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel">
+  <div className="carousel-inner">
+    <div className="carousel-item active">
+      <img src="https://www.runnea.com/archivos/202001/mejores-ofertas-adidas-mujer-portada-1200x572x80xX.png?1" class="d-block w-100" alt="..."></img>
+    </div>
+    <div className="carousel-item">
+      <img src="https://static.runnea.com/images/202002/12-mejores-ofertas-adidas-outlet-1200x572x80xX.jpg" class="d-block w-100" alt="..."></img>
+    </div>
+    <div className="carousel-item">
+      <img src="https://static.runnea.com/images/202005/descuentos-adidas-outlet-principal-726x900x80xX__101728.jpg" class="d-block w-100" alt="..."></img>
+    </div>
+  </div>
+</div>
+        
+    )
+}
---
+++ src/components/SearchBar/SearchBar.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Searchbar(){
+    return (
+      <form>
+        <input type="text" placeholder="Search..." />
+        <button type="submit"></button>
+      </form>
+    );
+}
