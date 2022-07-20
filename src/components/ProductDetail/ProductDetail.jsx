59126ac13706ee28f952ac570ca24c55fc9bdc8e
benjamalo <benjamalo02@gmail.com>
2022-07-20 11:28:38 -0300

    module.css de prodDetail

--- src/components/ProductDetail/ProductDetail.jsx
+++ src/components/ProductDetail/ProductDetail.jsx
@@ -2,6 +2,8 @@ import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getDetails } from "../../redux/actions";
 import { useParams } from "react-router-dom";
+import styles from "./ProductDetail.module.css";
+
 
 export default function ProductDetail(props) {
   const dispatch = useDispatch();
@@ -14,38 +16,46 @@ export default function ProductDetail(props) {
   const myShoes = useSelector((state) => state.detail);
   return (
     <div>
-      {
-            myShoes
-            ?<div>
-                <img src={myShoes.image} alt="imgShoes not found" />
-                <div>
-                <h1>{myShoes.title}</h1>
-                <h3>{myShoes.price}</h3>
-                <h5>descripcion del producto</h5>
-                </div>
-                <div>
-                    <h1>Size: </h1>
-                    <select>
-                        <option value="35">35</option>
-                        <option value="36">36</option>
-                        <option value="37">37</option>
-                        <option value="38">38</option>
-                        <option value="39">39</option>
-                        <option value="40">40</option>
-                        <option value="41">41</option>
-                        <option value="42">42</option>
-                        <option value="43">43</option>
-                        <option value="44">44</option>
-                    </select>
-                    <button>Comprar</button>
-                    <button>Añadir al carro</button>
-                </div>
+      {myShoes ? (
+        <div className={styles.divCard}>
+          <img
+            src={myShoes.image}
+            alt="imgShoes not found"
+          />
+          <div className={styles.divContent}>
+            <h1>{myShoes.title}</h1>
+            <h3>${myShoes.price}</h3>
+            <h5>descripcion del producto</h5>
+            <div className={styles.pop}>
+            <h1>Size: </h1>
+            <select>
+              <option value="35">35</option>
+              <option value="36">36</option>
+              <option value="37">37</option>
+              <option value="38">38</option>
+              <option value="39">39</option>
+              <option value="40">40</option>
+              <option value="41">41</option>
+              <option value="42">42</option>
+              <option value="43">43</option>
+              <option value="44">44</option>
+            </select>
             </div>
-            : <div>
-            <img src="https://i.pinimg.com/originals/76/59/35/7659353c8fcde74a4c224dafd7a5eccf.gif" alt="Shoes" />
-            <p>Loading...</p>
+            <div className={styles.buttons}>
+            <button className={styles}>Comprar</button>
+            <button className={styles}>Añadir al carro</button>
+            </div>
+          </div>
         </div>
-      }
+      ) : (
+        <div>
+          <img
+            src="https://i.pinimg.com/originals/76/59/35/7659353c8fcde74a4c224dafd7a5eccf.gif"
+            alt="Shoes"
+          />
+          <p>Loading...</p>
+        </div>
+      )}
     </div>
   );
 }
