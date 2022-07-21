2fa10f33c81a84428bfd5a21401e602924affd7a
benjamalo <benjamalo02@gmail.com>
2022-07-21 12:37:15 -0300

    ... en el name del zapato

--- src/components/ProductCard/ProductCard.jsx
+++ src/components/ProductCard/ProductCard.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
-
+import styles from "./ProductCard.module.css"
 export default function ProductCard({image, title, price}){
     return(
-        <div>
+        <div className={styles.card}>
             <img src={image} alt="img not found"/>
-            <div>
-                <h3>{title}</h3>
+            <div className={styles.container}>
+                <div className={styles.short}>
+                <h4 className={styles.ellipsis}>{title}<p>...</p></h4>
+                </div>
                 <p>${price}</p>
             </div>
         </div>
