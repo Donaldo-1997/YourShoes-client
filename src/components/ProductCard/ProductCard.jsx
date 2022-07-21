fd4c51c31eb29bf9aa8e36e1260e0a6ed2364693
benjamalo <benjamalo02@gmail.com>
2022-07-21 12:16:21 -0300

    Nombre del product...

--- src/components/ProductCard/ProductCard.jsx
+++ src/components/ProductCard/ProductCard.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
-
+import styles from "./ProductCard.module.css"
 export default function ProductCard({image, title, price}){
     return(
-        <div>
+        <div className={styles.card}>
             <img src={image} alt="img not found"/>
-            <div>
-                <h3>{title}</h3>
-                <p>{price}</p>
+            <div className={styles.container}>
+                <div className={styles.short}>
+                <h4 className={styles.ellipsis}>{title}<p>...</p></h4>
+                </div>
+                <p>${price}</p>
             </div>
         </div>
     )
