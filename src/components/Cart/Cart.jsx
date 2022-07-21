b69703c9de80aee932b47f19b2c9a6c784c17780
benjamalo <benjamalo02@gmail.com>
2022-07-21 17:53:35 -0300

    Creado el carrito, se agregan los productos

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,29 +1,28 @@
-import React from 'react'
+import React from "react";
+import styles from "./Cart.module.css"
+export default function Cart() {
+  const product = JSON.parse(localStorage.getItem("products"));
+  // const isArray = Array.isArray(product);
 
-
-export default function Cart(){
-
-    const product = JSON.parse(localStorage.getItem("products"))
-    // const isArray = Array.isArray(product)
-    console.log( product[0].title)
-   
-
-   
-
- return (
-   <div>
-    {
-        
-   product.forEach((prod) =>{
-    return (
-        <div>
-            <p>{prod.title}</p>
+  return (
+    <div>
+      {product ? (
+        product.map((prod) => {
+          return (
+            <div key={prod.id}>
+              <p>{prod.title}</p>
+              <img src={prod.image} alt="" />
+              <p>Price: ${prod.price}</p>
+              <button>Eliminar</button>
+            </div>
+          );
+        })
+      ) : (
+        <div className={styles.container2}>
+          <p>Tu carrito está vacío</p>
+          <p>Vuelve y escoge tus zapas favoritas</p>
         </div>
-    )
-
-   })
-        
-    }
-   </div>
- )
+      )}
+    </div>
+  );
 }
