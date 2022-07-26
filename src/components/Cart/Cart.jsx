adbfb650f3ae81819f0f2b56c03496b2ffafc79c
benjamalo <benjamalo02@gmail.com>
2022-07-26 00:34:11 -0300

    Cambios css de carrito

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,8 +1,9 @@
 import React, { useEffect } from "react";
-// import styles from "./Cart.module.css";
+import styles from "./Cart.module.css";
 import CartItem from "./CartItem.jsx";
 import { useSelector, useDispatch } from "react-redux/es/exports";
 import { deleteOneToCart, removerTodo } from "../../redux/actions";
+import { Link } from "react-router-dom";
 
 export default function Cart() {
   const cartProducts = useSelector((state) => state.cart);
@@ -11,10 +12,10 @@ export default function Cart() {
   console.log(cartProducts, "ESTO ES CART PRODUCTS!!!!");
 
   const deleteProduct = (id, all = false) => {
-    dispatch(deleteOneToCart({productId:id, all}));
+    dispatch(deleteOneToCart({ productId: id, all }));
   };
   const clearCart = () => {
-    dispatch(removerTodo())
+    dispatch(removerTodo());
   };
 
   useEffect(() => {
@@ -22,7 +23,12 @@ export default function Cart() {
   }, [cartProducts]);
 
   return (
-    <div>
+    <div className={styles.container}>
+      <Link to="/">
+        <button className={styles.yourshoes}>
+          YOUR<span className={styles.shoes}>SHOES</span>
+        </button>
+      </Link>
       <button onClick={clearCart}>Limpiar carrito</button>
       {cartProducts.length ? (
         cartProducts.map((item, index) => (
--- src/components/Cart/CartItem.jsx
+++ src/components/Cart/CartItem.jsx
@@ -1,11 +1,21 @@
+import styles from "./CartItem.module.css";
+
 const CartItem = ({ data, deleteProduct }) => {
-  let { id, image, price, quantity} = data;
+  let { id, image, price, quantity } = data;
   return (
-      <div>
-      <img src={image} alt="Img Not Found" width={100}/>
-      <h5>${price} x {quantity} = ${price*quantity}</h5>
-      <button onClick={()=> deleteProduct(id)}>Eliminar Uno</button>
-      <button onClick={()=> deleteProduct(id, true)}>Eliminar Todos</button>
+    <div className={styles.container}>
+      <img src={image} alt="Img Not Found" width={100} />
+      <div className={styles.info}>
+        <h5>
+          ${price} x {quantity} = ${price * quantity}
+        </h5>
+        <div className={styles.buttonsContainer}>
+          <button onClick={() => deleteProduct(id)}>Eliminar Uno</button>
+          <button onClick={() => deleteProduct(id, true)}>
+            Eliminar Todos
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
